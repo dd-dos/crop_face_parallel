53421c784791df033e60719b3051e3a6d6a8e48d
dd-dos <dungphamdang99@gmail.com>
2021-03-24 23:08:05 +0700

    fast commit

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ if __name__=="__main__":
         imgs, labels, imgs_id = data
         detector = torch.jit.load("./retinaface_torchscript/model/scripted_model.pt")
         with torch.no_grad():
-            batch_res = detector.forward_batch(imgs).cpu().numpy()
+            batch_res = detector.forward_batch(imgs)
 
         # pool = Pool(multiprocessing.cpu_count())
         pool = Pool(multiprocessing.cpu_count())
